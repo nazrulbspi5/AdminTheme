6b4b590b081b8419a8ff573eb0eb9b6fc62851ba
nazrulbspi5 <nazrulbspi5@gmail.com>
2022-09-23 12:39:24 +0600

    Added Admin Lte Theme

---
+++ AdminLte3.0.5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminLte3._0._5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
